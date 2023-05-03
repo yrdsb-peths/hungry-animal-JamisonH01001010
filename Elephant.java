78e0713da5dadb6225e4bce2e61c72ee95564d0e
JamisonH01001010 <349304303@gapps.yrdsb.ca>
2023-05-03 14:37:13 -0400

    Added background and elephant sound

--- Elephant.java
+++ Elephant.java
@@ -2,6 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Actor
 {
+    GreenfootSound elephantSound = new GreenfootSound("elephant.mp3");
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
@@ -20,6 +22,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
